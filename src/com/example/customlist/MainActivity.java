32021cc501d9a83e46dde9ca4dd990781392a7ea
Bergson Lima <bergsonsud@gmail.com>
2015-10-05 15:40:15 -0300

    Alteracao MainActivity - Request Done

--- src/com/example/customlist/MainActivity.java
+++ src/com/example/customlist/MainActivity.java
@@ -1,13 +1,13 @@
 package com.example.customlist;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,7 +16,6 @@ import android.view.MenuItem;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
@@ -32,38 +31,39 @@ public class MainActivity extends Activity {
     private String urlJsonArry = "http://bergsonlm.com/cliente";
     private ArrayList<Cliente> listaclients = new ArrayList<Cliente>();
 	private static String TAG = MainActivity.class.getSimpleName();
+	private ProgressDialog pDialog;
 	
-
-    //ArrayList prgmName;
-    
-       
-    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);        
         
-
-        
-//        makeJsonArrayRequest();
-                 
-
-
-        Cliente cliente = new Cliente("Berg", "");
-        listaclients.add(cliente);
-        
-                                   	
-        makeRequest();
-        context=this;
         lv=(ListView) findViewById(R.id.listView);
-        lv.setAdapter(new CustomAdapter(this, listaclients));
+        
+		pDialog = new ProgressDialog(this);
+		pDialog.setMessage("Please wait...");
+		pDialog.setCancelable(false);
+
+
+      
+        makeRequest();
+
         
     }
     
     
+	private void showpDialog() {
+		if (!pDialog.isShowing())
+			pDialog.show();
+	}
 
+	private void hidepDialog() {
+		if (pDialog.isShowing())
+			pDialog.dismiss();
+	}
 
     public void makeRequest(){
+    	showpDialog();
     	RequestQueue rq = Volley.newRequestQueue(this);
         JsonArrayRequest jReq = new JsonArrayRequest(urlJsonArry,
                 new Response.Listener<JSONArray>() {
@@ -86,6 +86,9 @@ public class MainActivity extends Activity {
                         }
                         //adpt.setItemList(result);
                         //adpt.notifyDataSetChanged();
+                        
+                        lv.setAdapter(new CustomAdapter(MainActivity.this, listaclients));
+                        hidepDialog();
                     }
 
 					
@@ -98,61 +101,11 @@ public class MainActivity extends Activity {
                     }
                 });
         rq.add(jReq);
+        
 
     }
 
-    private void makeJsonArrayRequest() {		
-
-		JsonArrayRequest req = new JsonArrayRequest(urlJsonArry,
-				new Response.Listener<JSONArray>() {
-					@Override
-					public void onResponse(JSONArray response) {
-						Log.d(TAG, response.toString());
-
-						try {
-							// Parsing json array response
-							// loop through each json object
-			
-							for (int i = 0; i < response.length(); i++) {
-
-								JSONObject person = (JSONObject) response
-										.get(i);
-
-								String nome = person.getString("nome");
-								String telefone = person.getString("telefone");
-								
-
-								Cliente cliente = new Cliente(nome, telefone);
-						        listaclients.add(cliente);
-							
-
-							}
-
-		//					txtResponse.setText(jsonResponse);
-
-						} catch (JSONException e) {
-							e.printStackTrace();
-							Toast.makeText(getApplicationContext(),
-									"Error: " + e.getMessage(),
-									Toast.LENGTH_LONG).show();
-						}
-
-	//					hidepDialog();
-					}
-				}, new Response.ErrorListener() {
-					@Override
-					public void onErrorResponse(VolleyError error) {
-						VolleyLog.d(TAG, "Error: " + error.getMessage());
-						Toast.makeText(getApplicationContext(),
-								error.getMessage(), Toast.LENGTH_SHORT).show();
-//						hidepDialog();
-					}
-				});
-
-		// Adding request to request queue
-	AppController.getInstance().addToRequestQueue(req);
-	}
-
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
